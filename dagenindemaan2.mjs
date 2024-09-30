7ba350082ee0640d5a952e7a4ce92240de64cdab
Reda901 <chreda40@gmail.com>
2024-09-30 15:02:38 +0200

    in orde !

--- dagenindemaan2.mjs
+++ dagenindemaan2.mjs
@@ -3,57 +3,28 @@ import{stdin as input, stdout as output} from 'node:process';
 import { log } from 'node:console';
 const userInput = readline.createInterface({input, output});
 
-let maand
+let artist = (await userInput.question("kies een artist   "))
 
-switch(maand){
+switch(artist){
 
-case 'Januari':
-console.log("in januari zijn er 31 dagen")
+case " Tupac":
+console.log("Al eyes on me") 
 break;
 
-case 'februari':
-console.log("in februari zijn er 28 of 29 dagen")
+case "lacrim":
+console.log("plata") 
 break;
 
-case 'maart':
-console.log("in maart zijn er 31 dagen")
+case "SCH":
+console.log("la nuit") 
 break;
 
-case 'april':
-console.log("in april zijn er 30 dagen")
+case "SDM":
+
+console.log("ocho") 
 break;
 
-case 'mei':
-console.log("in mei zijn er 31 dagen")
-break;
-
-case 'Juni':
-console.log("in juni zijn er 30 dagen")
-break;
-
-case 'Juli':
-console.log("in juli zijn er 31 dagen")
-break;
-
-case 'augustus':
-console.log("in augustus zijn er 31 dagen")
-break;
-
-case 'september':
-console.log("in september zijn er 30 dagen")
-break;
-
-case 'oktober':
-console.log("in oktober zijn er 31 dagen")
-break;
-
-case 'november':
-console.log("in november zijn er 30 dagen")
-break;
-
-case 'december':
-console.log("in januari zijn er 31 dagen")
-break;
+default:
 
 
 }
